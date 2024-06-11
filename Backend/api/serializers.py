7d0f16749423d48d3d8c00fc0d9897e9ada21043
= <=>
2024-06-11 12:35:48 +0530

    all models and api created

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -1,7 +1,40 @@
 from rest_framework import serializers
 from .models import Donor
+from .models import Seeker
+from .models import login_Donor
+from .models import login_Seeker
+from .models import contact_us
 
 class donorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Donor
         fields="__all__"
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        return Donor.objects.create(**validated_data)
+
+class seekerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Seeker
+        fields="__all__"
+
+class login_DomorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=login_Donor
+        fields="__all__"
+
+class login_SeekerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=login_Seeker
+        fields="__all__"  
+
+class contact_usSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=contact_us
+        fields = "__all__"                              
--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path,include
 from .views import donorViewSets
+from .views import seekerViewSets
+from .views import login_DonorViewSets
+from .views import login_SeekerViewSets
+from .views import contact_usViewSets
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'donor',donorViewSets)
+router.register(r'seeker',seekerViewSets)
+router.register(r'login_Donor',login_DonorViewSets)
+router.register(r'login_Seeker',login_SeekerViewSets)
+router.register(r'contact_us',contact_usViewSets)
+
 
 urlpatterns = [
         path('',include(router.urls))
